c3262b04b0158e24c82619582edc7cb17ed37b5d
Rafael Selvagio <137895450+RafaelSelvagio@users.noreply.github.com>
2023-11-20 19:13:35 -0300

    Update TarefaRepository.java

--- banco_exercicio02/src/main/java/br/com/spring/banco/repository/TarefaRepository.java
+++ banco_exercicio02/src/main/java/br/com/spring/banco/repository/TarefaRepository.java
@@ -3,11 +3,17 @@ package br.com.spring.banco.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import br.com.spring.banco.dto.TarefaDTO;
 import br.com.spring.banco.model.Tarefa;
 
 @Repository
 public interface TarefaRepository extends CrudRepository<Tarefa, Long> {
     
-	@Query("SELECT t.idTarefa, t.titulo, t.estado FROM Tarefa t")
+	@Query("SELECT t.idTarefa as idTarefa, t.titulo as titulo, t.estado as estado FROM Tarefa t")
 	Iterable<Tarefa> findTarefas();
+
+	// O resultado do JPQL cria objetos do tipo TarefaDTOImpl
+	@Query("SELECT new br.com.spring.banco.dto.TarefaDTOImpl(t.idTarefa, t.titulo, t.estado) FROM Tarefa t")
+	Iterable<TarefaDTO> findTarefasDTO();
 }
